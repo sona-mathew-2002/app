2a994b87ee581908540c53bc970f7ed88f2b8ab5
Sona Mathew <sonamathewc@gmail.com>
2024-08-24 11:10:34 +0530

    add navigation

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Homescreen';
 import * as Font from 'expo-font';
 import React, { useState, useEffect } from 'react';
@@ -9,7 +11,9 @@ import BottomNavBar from './components/BottomNavBar';
 import FinancePage from './FinancePage'
 import NewsPage from './NewsPage'
 import EntityPage from './Entity Page';
+import CommunicationPage from './CommunicationPage';
 
+const Stack = createNativeStackNavigator();
 // Prevent the splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
 
@@ -42,14 +46,23 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-      {/* <NewsPage/> */}
-      {/* <FinancePage/> */}
-      {/* <EntityPage/> */}
+    // <View style={styles.container}>
+
+      <>
+      <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="Homescreen">
+        <Stack.Screen name="Homescreen" component={HomeScreen} />
+        <Stack.Screen name="Finance" component={FinancePage} />
+        <Stack.Screen name="News" component={NewsPage} />
+        <Stack.Screen name="Communication" component={CommunicationPage} />
+        <Stack.Screen name="Entity" component={EntityPage} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
       <BottomNavBar/>
-    </View>
+    </NavigationContainer>
+      
+      </>
+  
   );
 }
 
@@ -59,5 +72,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily:'Radio Canada Big'
   },
 });
